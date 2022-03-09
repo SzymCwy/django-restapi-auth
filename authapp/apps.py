c4351b7daae98d58084b7761544e8e4cec584151
SzymCwy <43883129+SzymCwy@users.noreply.github.com>
2022-03-09 19:48:15 +0100

    Update apps.py

--- authapp/apps.py
+++ authapp/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
 
 
-<<<<<<< HEAD
-class authappConfig(AppConfig):
-=======
 class AuthappConfig(AppConfig):
->>>>>>> 5647d9303597d12ed639960286631e4ae957807f
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authapp'
